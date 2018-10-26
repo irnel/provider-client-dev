71f385aebb9c06015ca635eeec5395b25c43a172
irnel <irnel.victoria1986@gmail.com>
2018-10-26 17:30:19 -0400

    added error.interceptor

---
+++ src/app/helpers/error.interceptor.ts
@@ -0,0 +1,26 @@
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { AuthService } from '../services';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private readonly authService: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(err => {
+      if (err.status === 401) {
+        this.authService.logout();
+
+        location.reload(true);
+      }
+
+      const error = err.error.message || err.statusText;
+      return throwError(error);
+    }));
+  }
+}
