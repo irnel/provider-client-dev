74b60fe824afed6206b372dde4382b89d7106dd9
Irnel <irnel@radioguaimaro.icrt.cu>
2018-10-31 14:16:51 -0400

    added session service

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,3 +1,5 @@
+import { SessionService } from './../services';
+
 import { Observable } from 'rxjs';
 
 import { Injectable } from '@angular/core';
@@ -8,14 +10,17 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private readonly sessionService: SessionService
+  ) { }
 
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-      if (localStorage.getItem('currentUser')) {
+      if (this.sessionService.currentUserValue) {
         // logged in so return true
         return true;
       }
