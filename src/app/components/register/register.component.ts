56a00d7bf913a7d471519e26e8ea27710485046f
Irnel <irnel@radioguaimaro.icrt.cu>
2018-10-30 23:54:20 -0400

    added register component

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,57 @@
+import { first } from 'rxjs/operators';
+
+import { AlertService, UserService } from './../../services';
+
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private readonly userService: UserService,
+    private readonly alertService: AlertService,
+  ) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) { return; }
+
+    this.loading = true;
+    this.userService.register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(
+        () => {
+          this.alertService.success('', true);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        }
+      );
+  }
+
+}
