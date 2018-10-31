aab8fdbce11518d8f3b42883b5fa2a3b6783507a
Irnel <irnel@radioguaimaro.icrt.cu>
2018-10-30 23:55:59 -0400

    modified app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,56 @@
+import { JwtInterceptor, ErrorInterceptor, fakeBackendProvider, ContentTypeInterceptor } from './helpers';
+
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AngularFireModule } from 'angularfire2';
+import { ToastrModule, ToastContainerModule } from 'ngx-toastr';
 
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule,  } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AlertComponent } from './directives/alert/alert.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AlertComponent,
     LoginComponent,
-    AlertComponent
+    RegisterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+       positionClass: 'inline',
+       maxOpened: 1
+    }),
+    ToastContainerModule,
     MDBBootstrapModule.forRoot(),
     AngularFireModule.initializeApp(environment.fireBaseConfig)
   ],
   schemas: [
     NO_ERRORS_SCHEMA
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ContentTypeInterceptor, multi: true },
+
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
