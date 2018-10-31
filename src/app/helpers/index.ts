5f4d5f595bad52e929726c15ea90f0d0a554b24a
Irnel <irnel@radioguaimaro.icrt.cu>
2018-10-30 23:54:54 -0400

    modified index.ts

--- src/app/helpers/index.ts
+++ src/app/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './error.interceptor';
 export * from './jwt.interceptor';
 export * from './fake-backend';
+export * from './content-type.interceptor';
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,2 +1,3 @@
 export * from './alert/alert.service';
 export * from './auth/auth.service';
+export * from './user/user.service';
