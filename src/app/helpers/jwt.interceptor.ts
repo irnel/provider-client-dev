85ebdb6d6bfa99ccd7d38564a0d33c9cdcb1b9b3
Irnel <irnel@radioguaimaro.icrt.cu>
2018-10-30 23:56:26 -0400

    modified jwt.interceptor

--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -5,12 +5,11 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  private localStorage: Storage;
 
   constructor() {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
     if (currentUser && currentUser.token) {
       req = req.clone({
