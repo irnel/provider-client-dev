d6c1bf33318e674104359efa56695048fa4dcf95
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-01 12:04:27 -0400

    added observable

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,22 +1,36 @@
 import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { User } from '../../models';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
-  login(username: string, password: string) {
+  get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(email: string, password: string) {
     return this.http.post<any>(
-      `/users/authenticate`, { username: username, password: password })
+      `/users/authenticate`, { email, password })
       .pipe(map(user => {
 
         if (user && user.token) {
           localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
         }
 
         return user;
@@ -25,5 +39,6 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
   }
 }
