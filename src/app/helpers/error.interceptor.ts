fee9e598c7b0ad898b4b91939e20fa4f5083325f
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-01 12:04:56 -0400

    modified error.interceptor

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -14,8 +14,8 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(catchError(err => {
       if (err.status === 401) {
+        // auto logout if 401 response returned from api
         this.authService.logout();
-
         location.reload(true);
       }
 
