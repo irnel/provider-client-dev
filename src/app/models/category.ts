3d924e426584e3451c4e6e2e2df9105c668d68b2
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-06 10:12:31 -0500

    modified category

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,7 +1,7 @@
 export class Category {
 
   constructor(
-    public restaurantId: string,
+    public providerId: string,
     public name: string,
     public description: string,
     public image: string,
