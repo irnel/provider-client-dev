4761d737741e933554fdc16817371429dada1016
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-09 19:23:25 -0500

    updated navigation.module

--- src/app/components/dashboards/provider/main-layout/navigation/navigation.module.ts
+++ src/app/components/dashboards/provider/main-layout/navigation/navigation.module.ts
@@ -1,8 +1,18 @@
+
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
+} from '@angular/material';
 
 import { NavigationComponent } from './navigation.component';
 
@@ -11,6 +21,12 @@ import { NavigationComponent } from './navigation.component';
     CommonModule,
     FormsModule,
     RouterModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
     MDBBootstrapModule.forRoot()
   ],
   declarations: [
