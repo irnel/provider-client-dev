68655408d12038da44f9408bf77b62e453fa62b0
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-16 11:54:08 -0500

    updated views.module

--- src/app/components/dashboards/provider/views/views.module.ts
+++ src/app/components/dashboards/provider/views/views.module.ts
@@ -1,15 +1,24 @@
-import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
-import { StatsCardComponent } from './stats-card/stats-card.component';
-import { StatsCard1Component } from './stats-card1/stats-card1.component';
-import { ChartsComponent } from './charts/charts.component';
-import { BasicTableComponent } from './tables/basic-table/basic-table.component';
+import {
+  StatsCardComponent,
+  StatsCard1Component,
+  ChartsComponent,
+  BasicTableComponent,
+  HomeWorkspaceComponent,
+  ProviderWorkspaceComponent,
+  ProductWorkspaceComponent,
+  CategoryWorkspaceComponent,
+  CashierWorkspaceComponent,
+  OrderWorkspaceComponent
+} from './index';
 
 
 @NgModule({
@@ -25,13 +34,25 @@ import { BasicTableComponent } from './tables/basic-table/basic-table.component'
     StatsCardComponent,
     StatsCard1Component,
     ChartsComponent,
-    BasicTableComponent
+    BasicTableComponent,
+    HomeWorkspaceComponent,
+    ProviderWorkspaceComponent,
+    ProductWorkspaceComponent,
+    CategoryWorkspaceComponent,
+    CashierWorkspaceComponent,
+    OrderWorkspaceComponent
   ],
   exports: [
     StatsCardComponent,
     StatsCard1Component,
     ChartsComponent,
-    BasicTableComponent
+    BasicTableComponent,
+    HomeWorkspaceComponent,
+    ProviderWorkspaceComponent,
+    ProductWorkspaceComponent,
+    CategoryWorkspaceComponent,
+    CashierWorkspaceComponent,
+    OrderWorkspaceComponent
   ],
   schemas: [
     NO_ERRORS_SCHEMA,
