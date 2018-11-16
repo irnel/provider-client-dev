bdcc42aa20edf381a4f539ef9a5fb01147893feb
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-16 11:57:17 -0500

    added index.ts

---
+++ src/app/components/dashboards/provider/views/index.ts
@@ -0,0 +1,12 @@
+export * from './home-workspace/home-workspace.component';
+export * from './provider-workspace/provider-workspace.component';
+export * from './product-workspace/product-workspace.component';
+export * from './category-workspace/category-workspace.component';
+export * from './cashier-workspace/cashier-workspace.component';
+export * from './order-workspace/order-workspace.component';
+
+export * from './charts/charts.component';
+export * from './stats-card/stats-card.component';
+export * from './stats-card1/stats-card1.component';
+export * from './tables/basic-table/basic-table.component';
+
