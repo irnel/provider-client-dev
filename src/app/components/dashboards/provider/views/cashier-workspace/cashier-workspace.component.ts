f3c1b665c056b63e4ff883203e9fc4bc0d0f7266
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-16 11:50:10 -0500

    added cashier-workspace

---
+++ src/app/components/dashboards/provider/views/cashier-workspace/cashier-workspace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cashier-workspace',
+  templateUrl: './cashier-workspace.component.html',
+  styleUrls: ['./cashier-workspace.component.scss']
+})
+export class CashierWorkspaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
