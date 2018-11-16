1c614d5b369b2a26f3283b81c64c58ff8e437fcf
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-16 11:52:53 -0500

    added product-workspace component

---
+++ src/app/components/dashboards/provider/views/product-workspace/product-workspace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-workspace',
+  templateUrl: './product-workspace.component.html',
+  styleUrls: ['./product-workspace.component.scss']
+})
+export class ProductWorkspaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
