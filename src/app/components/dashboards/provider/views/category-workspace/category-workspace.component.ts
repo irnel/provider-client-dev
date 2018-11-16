4d1e7b89f30a6fb25d435e8341e1ee9a6d3a788a
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-16 11:50:37 -0500

    added category-workspace component

---
+++ src/app/components/dashboards/provider/views/category-workspace/category-workspace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-workspace',
+  templateUrl: './category-workspace.component.html',
+  styleUrls: ['./category-workspace.component.scss']
+})
+export class CategoryWorkspaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
