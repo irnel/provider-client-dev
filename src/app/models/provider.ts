60bf66fbac275edd65b4ddd5456cabb5c9b3320e
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-27 07:55:32 -0500

    Updated provider model

--- src/app/models/provider.ts
+++ src/app/models/provider.ts
@@ -1,2 +1,14 @@
+import { Address } from './index';
+
 export class Provider {
+
+  constructor(
+    public name: string,
+    public description: string,
+    public address: Address,
+    public image?: string,
+    public userId?: string,
+    public id?: string
+  ) {}
+
 }
