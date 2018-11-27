b27c4a42734e7aee4be51b944045a190bf22f392
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-27 07:53:05 -0500

    Updated home-provider.module

--- src/app/components/dashboards/provider/home-provider/home-provider.module.ts
+++ src/app/components/dashboards/provider/home-provider/home-provider.module.ts
@@ -1,13 +1,14 @@
-
 import { NavigationModule } from './../main-layout/navigation/navigation.module';
 import { RouterModule } from '@angular/router';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { SharedModule } from './../../shared/shared.module';
 import { ViewsModule } from './../views/views.module';
+import { MaterialModule } from '../../../material/material.module';
 
 import { HomeProviderComponent } from './home-provider.component';
 
@@ -19,6 +20,8 @@ import { HomeProviderComponent } from './home-provider.component';
     NavigationModule,
     ViewsModule,
     SharedModule,
+    MaterialModule,
+    FlexLayoutModule,
     MDBBootstrapModule.forRoot(),
   ],
   declarations: [
