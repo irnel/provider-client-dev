655c90adf9ca7a7f6e6e084e88bd348b3187a8c4
Irnel <irnel@radioguaimaro.icrt.cu>
2018-11-27 07:55:45 -0500

    Updated shared.module

--- src/app/components/dashboards/shared/shared.module.ts
+++ src/app/components/dashboards/shared/shared.module.ts
@@ -4,9 +4,12 @@ import { RouterModule } from '@angular/router';
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { NavigationModule } from '../provider/main-layout/navigation/navigation.module';
+import { MaterialModule } from './../../material/material.module';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
@@ -14,13 +17,18 @@ import { FooterComponent } from './footer/footer.component';
     FormsModule,
     RouterModule,
     NavigationModule,
+    MaterialModule,
     MDBBootstrapModule.forRoot(),
   ],
   declarations: [
-    FooterComponent
+    LayoutComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   exports: [
-    FooterComponent
+    LayoutComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   schemas: [
     NO_ERRORS_SCHEMA,
