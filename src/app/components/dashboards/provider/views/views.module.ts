225687c8bb0fdd43460ddb033353f467cffb1765
Irnel <irnel@radioguaimaro.icrt.cu>
2018-12-01 12:15:26 -0500

    updated views.module

--- src/app/components/dashboards/provider/views/views.module.ts
+++ src/app/components/dashboards/provider/views/views.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,14 +22,14 @@ import {
 } from './index';
 import { EditProviderWorkspaceComponent } from './edit-provider-workspace/edit-provider-workspace.component';
 
-
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule,
     FormsModule,
+    RouterModule,
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MaterialModule,
     MDBBootstrapModule.forRoot()
   ],
