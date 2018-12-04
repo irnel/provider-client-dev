398f949841b4766c4f5ce58e6c55c34bd7cc313c
Irnel <irnel@radioguaimaro.icrt.cu>
2018-12-03 21:39:08 -0500

    Updated edit-provider-workspace.component

--- src/app/components/dashboards/provider/views/edit-provider-workspace/edit-provider-workspace.component.ts
+++ src/app/components/dashboards/provider/views/edit-provider-workspace/edit-provider-workspace.component.ts
@@ -1,7 +1,11 @@
+/// <reference types="@types/googlemaps" />
+
+import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, NgZone } from '@angular/core';
 
 import { Address, Provider } from '../../../../../models';
+import { MapsAPILoader } from '@agm/core';
 
 
 @Component({
@@ -15,13 +19,19 @@ export class EditProviderWorkspaceComponent implements OnInit {
   public address: Address;
   public provider: Provider;
 
+  @ViewChild('search') search: ElementRef;
+
   constructor(
-    private formBuilder: FormBuilder
+    private router: Router,
+    private ngZone: NgZone,
+    private formBuilder: FormBuilder,
+    private mapsAPILoader: MapsAPILoader
   ) { }
 
   ngOnInit() {
     this.editForm = this.formBuilder.group({
       name: ['', Validators.required],
+      address: ['', Validators.required]
     });
 
     // google maps values
@@ -33,12 +43,58 @@ export class EditProviderWorkspaceComponent implements OnInit {
       formattedAddress: ''
     };
 
+     // set current position
+     this.setCurrentPosition();
+
+    // load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      const autocomplete = new google.maps.places.Autocomplete(this.search.nativeElement, {
+        types: ['address']
+      });
+
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          // get the place result
+          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          this.address.formattedAddress = place.formatted_address;
+
+          // verify result
+          if (place.geometry === undefined || !place.geometry) {
+            return;
+          }
+
+          // set latitude, longitude and zoom
+          this.address.lat = place.geometry.location.lat();
+          this.address.lng = place.geometry.location.lng();
+          this.zoom = 12;
+
+          this.provider.address = this.address;
+        });
+      });
+    });
   }
 
   get form() { return this.editForm.controls; }
 
-  placeMarker(position: any) {
-    this.address.lat = position.coords.lat;
-    this.address.lng = position.coords.lng;
+  private setCurrentPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.address.lat = position.coords.latitude;
+        this.address.lng = position.coords.longitude;
+        this.zoom = 8;
+
+        this.provider.address = this.address;
+      });
+    }
   }
+
+  private redirectToProviderWorkspace() {
+    this.router.navigate(['/provider-dashboard/workspace/providers']);
+  }
+
+  cancel() {
+    this.redirectToProviderWorkspace();
+  }
+
 }
