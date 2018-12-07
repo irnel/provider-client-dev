f7aee11e947c62fc246b3f16558bf0f88fdfd3db
Irnel <irnel@radioguaimaro.icrt.cu>
2018-12-06 20:49:32 -0500

    Updated index.ts

--- src/app/components/dashboards/provider/views/index.ts
+++ src/app/components/dashboards/provider/views/index.ts
@@ -4,6 +4,7 @@ export * from './product-workspace/product-workspace.component';
 export * from './category-workspace/category-workspace.component';
 export * from './cashier-workspace/cashier-workspace.component';
 export * from './order-workspace/order-workspace.component';
+export * from './edit-provider-workspace/edit-provider-workspace.component';
 
 export * from './charts/charts.component';
 export * from './stats-card/stats-card.component';
