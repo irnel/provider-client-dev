bbd45e94abc1e4aefe689eb92e9dbff3ae4e0b45
Irnel <irnel@radioguaimaro.icrt.cu>
2018-12-14 10:42:08 -0500

    Updated edit-category-workspace.component

--- src/app/components/dashboards/provider/views/edit-category-workspace/edit-category-workspace.component.ts
+++ src/app/components/dashboards/provider/views/edit-category-workspace/edit-category-workspace.component.ts
@@ -1,10 +1,14 @@
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { Provider, PROVIDERS_DATA } from './../../../../../helpers';
 import { Observable } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
+import { ToastrService, ToastContainerDirective } from 'ngx-toastr';
+
+import { Provider, Category, PROVIDERS_DATA } from './../../../../../helpers';
+import { Config } from './../../../../../infrastructure';
+
 
 @Component({
   selector: 'app-edit-category-workspace',
@@ -12,17 +16,26 @@ import { startWith, map } from 'rxjs/operators';
   styleUrls: ['./edit-category-workspace.component.scss']
 })
 export class EditCategoryWorkspaceComponent implements OnInit {
+  @ViewChild(ToastContainerDirective) toastContainer: ToastContainerDirective;
   editForm: FormGroup;
   edit: boolean;
   title: string;
+  regEx: string = Config.regex[0];
+  regEx1: string = Config.regex[1];
 
   providers: Provider[] = PROVIDERS_DATA;
   filteredProviders: Observable<Provider[]>;
+  currentProviders: Provider[];
+
+  providerNotFound = true;
+  inputProvider: any;
+
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private readonly toast: ToastrService
   ) {
     // Change Form values
     this.route.data.subscribe(data => {
@@ -39,17 +52,38 @@ export class EditCategoryWorkspaceComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.toast.overlayContainer = this.toastContainer;
+
     this.editForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      providerName: ['', Validators.nullValidator],
+      name: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(this.regEx)]
+      )],
+      providerName: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(this.regEx)]
+      )],
       image: ['', Validators.nullValidator],
-      description: ['', Validators.nullValidator]
+      description: ['', Validators.pattern(this.regEx1)]
     });
 
-    this.filteredProviders = this.form.providerName.valueChanges
+    this.inputProvider = this.form.providerName;
+
+    this.form.providerName.valueChanges
       .pipe(
         startWith(''),
-        map(name => name ? this.elementFilter(name) : this.providers.slice())
+        map(name => {
+          if (name) {
+            this.providerNotFound = false;
+            return this.elementFilter(name);
+          } else {
+            this.providerNotFound = true;
+            return this.providers.slice();
+          }
+        }
+      ))
+      .subscribe(
+        list => this.currentProviders = list
       );
   }
 
@@ -58,6 +92,7 @@ export class EditCategoryWorkspaceComponent implements OnInit {
   MarkAsDirty() {
     if (this.editForm.invalid) {
       this.form.name.markAsDirty();
+      this.form.providerName.markAsDirty();
 
       return;
     }
@@ -88,6 +123,20 @@ export class EditCategoryWorkspaceComponent implements OnInit {
     return this.providers.filter(provider =>
       provider.name.toLowerCase().indexOf(filterValue) === 0
     );
+  }
+
+  showSuccessMessage(message: string, time: number) {
+    this.toast.success(message, '', {
+      tapToDismiss: true,
+      timeOut: time
+    });
+  }
+
+  showErrorMessage(message: string, time: number) {
+    this.toast.error(message, '', {
+      tapToDismiss: true,
+      timeOut: time
+    });
   }
 
 
