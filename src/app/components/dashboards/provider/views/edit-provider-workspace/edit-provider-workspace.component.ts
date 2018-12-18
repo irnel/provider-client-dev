ab5dd3ae5453afecac82789c843a370149ab7e20
Irnel <irnel@radioguaimaro.icrt.cu>
2018-12-17 20:28:46 -0500

    Updated edit-provider-workspace.component

--- src/app/components/dashboards/provider/views/edit-provider-workspace/edit-provider-workspace.component.ts
+++ src/app/components/dashboards/provider/views/edit-provider-workspace/edit-provider-workspace.component.ts
@@ -4,8 +4,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit, ElementRef, ViewChild, NgZone } from '@angular/core';
 
+import { startWith, map } from 'rxjs/operators';
 import { Address, Provider } from '../../../../../models';
 import { MapsAPILoader } from '@agm/core';
+import { Config } from '../../../../../infrastructure';
+import { SnotifyService } from 'ng-snotify';
 
 @Component({
   selector: 'app-edit-provider-workspace',
@@ -22,12 +25,20 @@ export class EditProviderWorkspaceComponent implements OnInit {
   title: string;
   edit: boolean;
 
+  test: string;
+
+  regEx: string = Config.regex[0];
+  regEx1: string = Config.regex[1];
+  nameError: string;
+  addressError: string;
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private ngZone: NgZone,
     private formBuilder: FormBuilder,
-    private mapsAPILoader: MapsAPILoader
+    private readonly mapsAPILoader: MapsAPILoader,
+    private readonly toast: SnotifyService
   ) {
 
     this.route.data.subscribe(data => {
@@ -40,15 +51,55 @@ export class EditProviderWorkspaceComponent implements OnInit {
         this.edit = true;
 
       }
-
     });
   }
 
   ngOnInit() {
     this.editForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      address: ['', Validators.required]
+      name: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(this.regEx)]
+      )],
+      address: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(this.regEx)]
+      )],
+      description: ['', Validators.pattern(this.regEx1)]
     });
+
+    // validate name
+    this.form.name.valueChanges.pipe(
+      startWith(''),
+      map(() => {
+        let error = '';
+        if (this.form.name.hasError('required')) {
+          error = 'name is required';
+        }
+
+        if (this.form.name.hasError('pattern')) {
+          error = 'only letters and numbers are allowed';
+        }
+
+        return error;
+      })
+    ).subscribe(error => this.nameError = error);
+
+    // validate Address
+    this.form.address.valueChanges.pipe(
+      startWith(''),
+      map(() => {
+        let error = '';
+        if (this.form.address.hasError('required')) {
+          error = 'address is required';
+        }
+
+        if (this.form.address.hasError('pattern')) {
+          error = 'only letters and numbers are allowed';
+        }
+
+        return error;
+      })
+    ).subscribe(error => this.addressError = error);
 
     // google maps values
     this.zoom = 4;
@@ -72,11 +123,12 @@ export class EditProviderWorkspaceComponent implements OnInit {
         this.ngZone.run(() => {
           // get the place result
           const place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
           this.address.formattedAddress = place.formatted_address;
 
           // verify result
           if (place.geometry === undefined || !place.geometry) {
+            // reset formattedAddress
+            this.address.formattedAddress = '';
             return;
           }
 
@@ -84,7 +136,6 @@ export class EditProviderWorkspaceComponent implements OnInit {
           this.address.lat = place.geometry.location.lat();
           this.address.lng = place.geometry.location.lng();
           this.zoom = 12;
-
         });
       });
     });
@@ -116,6 +167,21 @@ export class EditProviderWorkspaceComponent implements OnInit {
       this.form.name.markAsDirty();
       this.form.address.markAsDirty();
 
+      // validate address
+      if (this.address.formattedAddress === '' && this.form.address.value !== '') {
+        this.showErrorMessage(
+          'Validation error',
+          'select a valid address from the list.', 2500);
+      }
+
+      // validate description
+      if (this.form.description.hasError('pattern')) {
+        this.showErrorMessage(
+          'Validation error',
+          'Character not allowed. ' +
+          'Only letters and numbers are allowed', 2500
+        );
+      }
       return;
     }
   }
@@ -130,6 +196,15 @@ export class EditProviderWorkspaceComponent implements OnInit {
     } else { // edit
 
     }
+  }
+
+  showErrorMessage(title: string, body: string, timeOut: number) {
+    this.toast.error(body, title, {
+      timeout: timeOut,
+      showProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true
+    });
   }
 
 }
