01622261bb801bf998ec3f15ee9bd80838ed1cd9
Irnel <irnel@radioguaimaro.icrt.cu>
2019-01-08 15:57:00 -0500

    Added component

---
+++ src/app/components/dashboards/provider/views/product-details-workspace/product-details-workspace.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Provider, Product, Category, PROVIDERS_DATA, CATEGORY_DATA, PRODUCT_DATA } from '../../../../../helpers';
+
+@Component({
+  selector: 'app-product-details-workspace',
+  templateUrl: './product-details-workspace.component.html',
+  styleUrls: ['./product-details-workspace.component.scss']
+})
+export class ProductDetailsWorkspaceComponent implements OnInit {
+
+  providers: Provider [] = PROVIDERS_DATA;
+  categories: Category [] = CATEGORY_DATA;
+  products: Product [] = PRODUCT_DATA;
+
+  currentProduct: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    const prodId = +this.route.snapshot.params['prodId'];
+    this.currentProduct = this.products.find(p => p.id === prodId);
+  }
+
+}
