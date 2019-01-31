dd56f7843e110c21e11889dc1a68580c78698e85
Irnel <irnel@radioguaimaro.icrt.cu>
2019-01-30 22:19:40 -0500

    updated component

--- src/app/components/dashboards/provider/views/file-input/file-input.component.ts
+++ src/app/components/dashboards/provider/views/file-input/file-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { FileInfo } from '../../../../../helpers';
 
 @Component({
@@ -8,30 +8,74 @@ import { FileInfo } from '../../../../../helpers';
 })
 export class FileInputComponent implements OnInit {
   @Output() public filesInfo = new EventEmitter<FileInfo []>();
+  @ViewChild('alert') alert: ElementRef;
   selectedFiles: FileInfo [] = [];
+  showError = false;
+  fileName = '';
+  errorMsg = '';
+  extensionError = false;
+  sizeError = false;
+  maxAllowed = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  closeAlert() {
+    this.alert.nativeElement.classList.remove('show');
+    this.showError = false;
+    this.extensionError = false;
+    this.sizeError = false;
+    this.maxAllowed = false;
+    this.fileName = '';
+  }
+
   onSelectFile(event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+    // multiple files
+    for (const file of event.target.files) {
+      const reader = new FileReader();
 
-    reader.addEventListener('load', (eventProgress: any) => {
-      const index = this.selectedFiles.findIndex(f => f.file.name === file.name);
-      if (index === -1) {
-        const fileInfo = new FileInfo(eventProgress.target.result, file);
-        fileInfo.pending = true;
-        this.selectedFiles.push(fileInfo);
+      // validate max number of image allowed
+      if (this.selectedFiles.length + event.target.files.length > 5) {
+        this.showError = true;
+        this.maxAllowed = true;
 
-        // event send fileInfo data
-        this.filesInfo.emit(this.selectedFiles);
+        return;
       }
-    });
 
-    reader.readAsDataURL(file);
+      // validate image extension
+      if (!this.isImageExtension(file.name)) {
+        this.showError = true;
+        this.extensionError = true;
+        this.fileName = file.name;
+
+        return;
+      }
+
+      // validate image size
+      if (!this.isValidImageSize(file.size)) {
+        this.showError = true;
+        this.sizeError = true;
+        this.fileName = file.name;
+
+        return;
+      }
+
+      reader.addEventListener('load', (eventProgress: any) => {
+        const index = this.selectedFiles.findIndex(f => f.file.name === file.name);
+        if (index === -1) {
+          const fileInfo = new FileInfo(eventProgress.target.result, file);
+          fileInfo.pending = true;
+          this.selectedFiles.push(fileInfo);
+        }
+      });
+
+      reader.readAsDataURL(file);
+    }
+
+    // event send fileInfo data
+    this.filesInfo.emit(this.selectedFiles);
   }
 
   markAsPrincipal(image: FileInfo) {
@@ -52,10 +96,24 @@ export class FileInputComponent implements OnInit {
     const index = this.selectedFiles.findIndex(f => f.file.name === image.file.name);
     const fileInfoRemoved = this.selectedFiles.splice(index, 1)[0];
 
-    // select the previous one as principal
+    // select default element as principal
     // if the deleted item was marked as principal
     if (this.selectedFiles.length > 0 && fileInfoRemoved.markAsPrincipal) {
-      this.selectedFiles[index - 1].markAsPrincipal = true;
+      index > 0
+      ? this.selectedFiles[index - 1].markAsPrincipal = true
+      : this.selectedFiles[index].markAsPrincipal = true;
     }
+  }
+
+  // valid image extension "jpg", "jpeg", "gif", "png"
+  isImageExtension(fileName: string) {
+    const regex = new RegExp('(.*?)\.(jpg|jpeg|gif|png)$');
+
+    return regex.test(fileName);
+  }
+
+  // file size > 2048 kb
+  isValidImageSize(size: number) {
+    return (size / 1024) <= 2048;
   }
 }
