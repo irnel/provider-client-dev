412c2cb2695016868fa8d11b9325708ebe777f4c
Irnel <irnel@radioguaimaro.icrt.cu>
2019-02-07 21:08:00 -0500

    Updated routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HomeProviderComponent } from './components/dashboards/provider/home-provider/home-provider.component';
 
@@ -28,30 +27,29 @@ import {
 const routes: Routes = [
 
   // Auth routing
-  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/sign-in', component: LoginComponent },
   { path: 'auth/register-user', component: RegisterComponent },
-  { path: 'auth/forgot-password', component: ForgotPasswordComponent },
 
   // Provider Dashboard
-  { path: 'provider-dashboard/workspace', component: HomeProviderComponent,
+  { path: 'provider-dashboard/workspace', component: HomeProviderComponent, canActivate: [AuthGuard],
     children: [
       { path: 'home', component: HomeWorkspaceComponent },
       { path: 'providers', component: ProviderWorkspaceComponent },
       { path: 'providers/create', component: EditProviderWorkspaceComponent, data: { mode: 'create' }},
       { path: 'providers/:id/edit', component: EditProviderWorkspaceComponent, data: { mode: 'edit' }},
-      { path: 'providers/:id/details', component: ProviderDetailsWorkspaceComponent },
-      { path: 'providers/:id/categories', component: CategoryWorkspaceComponent },
-      { path: 'providers/:id/categories/create', component: EditCategoryWorkspaceComponent, data: { mode: 'create' }},
-      { path: 'providers/:id/categories/:catId/edit', component: EditCategoryWorkspaceComponent, data: { mode: 'edit' }},
-      { path: 'providers/:id/categories/:catId/details', component: CategoryDetailsWorkspaceComponent },
-      { path: 'providers/:id/cashiers', component: CashierWorkspaceComponent },
-      { path: 'providers/:id/cashiers/create', component: EditCashierWorkspaceComponent, data: { mode: 'create' }},
-      { path: 'providers/:id/cashiers/:cashId/edit', component: EditCashierWorkspaceComponent, data: { mode: 'edit' }},
-      { path: 'providers/:id/categories/:catId/products', component: ProductWorkspaceComponent },
-      { path: 'providers/:id/categories/:catId/products/create', component: EditProductWorkspaceComponent, data: { mode: 'create' }},
-      { path: 'providers/:id/categories/:catId/products/:prodId/edit', component: EditProductWorkspaceComponent, data: { mode: 'edit' }},
-      { path: 'providers/:id/categories/:catId/products/:prodId/details', component: ProductDetailsWorkspaceComponent },
-      { path: 'orders', component: OrderWorkspaceComponent },
+      // { path: 'providers/:id/details', component: ProviderDetailsWorkspaceComponent },
+      // { path: 'providers/:id/categories', component: CategoryWorkspaceComponent },
+      // { path: 'providers/:id/categories/create', component: EditCategoryWorkspaceComponent, data: { mode: 'create' }},
+      // { path: 'providers/:id/categories/:catId/edit', component: EditCategoryWorkspaceComponent, data: { mode: 'edit' }},
+      // { path: 'providers/:id/categories/:catId/details', component: CategoryDetailsWorkspaceComponent },
+      // { path: 'providers/:id/cashiers', component: CashierWorkspaceComponent },
+      // { path: 'providers/:id/cashiers/create', component: EditCashierWorkspaceComponent, data: { mode: 'create' }},
+      // { path: 'providers/:id/cashiers/:cashId/edit', component: EditCashierWorkspaceComponent, data: { mode: 'edit' }},
+      // { path: 'providers/:id/categories/:catId/products', component: ProductWorkspaceComponent },
+      // { path: 'providers/:id/categories/:catId/products/create', component: EditProductWorkspaceComponent, data: { mode: 'create' }},
+      // { path: 'providers/:id/categories/:catId/products/:prodId/edit', component: EditProductWorkspaceComponent, data: { mode: 'edit' }},
+      // { path: 'providers/:id/categories/:catId/products/:prodId/details', component: ProductDetailsWorkspaceComponent },
+      // { path: 'orders', component: OrderWorkspaceComponent },
 
       // redirect to workspace home by default
       { path: '', redirectTo: 'home', pathMatch: 'full' }
