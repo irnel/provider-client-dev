960f55d1abcf60b5a6ddd0888c0e6ff89fbc74c2
Irnel <irnel@radioguaimaro.icrt.cu>
2019-07-02 10:58:14 -0400

    Update model

--- src/app/models/provider.ts
+++ src/app/models/provider.ts
@@ -3,6 +3,8 @@ import { Address } from './address';
 export interface Provider {
   name: string;
   address: Address;
+  phone?: string;
+  schedule?: string [];
   description?: string;
   url?: string;
   userId?: string;
